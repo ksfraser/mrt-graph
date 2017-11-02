78df55509015aedea518f32919ee27a1cda947b7
Ruhong <seahrh@gmail.com>
2017-11-03 05:31:27 +0800

    Complete EWL graph

--- src/main/scala/com/sgcharts/mrt/graph/EastWestLine.scala
+++ src/main/scala/com/sgcharts/mrt/graph/EastWestLine.scala
@@ -7,11 +7,39 @@ import scalax.collection.edge.Implicits._
 object EastWestLine {
   val name: String = "ewl"
   val graph: Graph[Station, WUnDiEdge] = Graph(
-    (PasirRis ~% Tampines)(2),
-    (Tampines ~% Simei)(3)
+    (PasirRis ~% Tampines) (3),
+    (Tampines ~% Simei) (3),
+    (TanahMerah ~% Simei) (3),
+    (TanahMerah ~% Expo) (3),
+    (ChangiAirport ~% Expo) (5),
+    (TanahMerah ~% Bedok) (3),
+    (Kembangan ~% Bedok) (3),
+    (Kembangan ~% Eunos) (2),
+    (PayaLebar ~% Eunos) (2),
+    (PayaLebar ~% Aljunied) (2),
+    (Kallang ~% Aljunied) (2),
+    (Kallang ~% Lavender) (2),
+    (Bugis ~% Lavender) (2),
+    (Bugis ~% CityHall) (2),
+    (RafflesPlace ~% CityHall) (2),
+    (RafflesPlace ~% TanjongPagar) (2),
+    (OutramPark ~% TanjongPagar) (2),
+    (OutramPark ~% TiongBahru) (3),
+    (Redhill ~% TiongBahru) (2),
+    (Redhill ~% Queenstown) (3),
+    (Commonwealth ~% Queenstown) (2),
+    (Commonwealth ~% BuonaVista) (2),
+    (Dover ~% BuonaVista) (2),
+    (Dover ~% Clementi) (3),
+    (JurongEast ~% Clementi) (4),
+    (JurongEast ~% ChineseGarden) (3),
+    (Lakeside ~% ChineseGarden) (2),
+    (Lakeside ~% BoonLay) (3),
+    (Pioneer ~% BoonLay) (1),
+    (Pioneer ~% JooKoon) (4),
+    (GulCircle ~% JooKoon) (3),
+    (GulCircle ~% TuasCrescent) (3),
+    (TuasWestRoad ~% TuasCrescent) (2),
+    (TuasWestRoad ~% TuasLink) (3),
   )
-  
-  def main(args: Array[String]): Unit = {
-    println(graph)
-  }
 }
